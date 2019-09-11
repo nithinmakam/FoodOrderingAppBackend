cfeebe30937e851fa8b35017e9985cb55fddc08c
emansi <mansielhance@gmail.com>
2019-09-12 01:45:16 +0530

    Sign up Endpoint - Completed Created, CustomerDao, CustomerController, SignUpBusinessService

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -1,4 +1,45 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
+import com.upgrad.FoodOrderingApp.service.businness.SignUpBusinessService;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
 public class CustomerController {
+
+
+    @Autowired
+    private SignUpBusinessService signupBusinessService;
+
+    //1. signup endpoint
+    @RequestMapping(method = RequestMethod.POST, path="/customer/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupCustomerResponse> signup(final SignupCustomerRequest signupCustomerRequest) throws SignUpRestrictedException
+    {
+        CustomerEntity customerEntity = new CustomerEntity();
+        customerEntity.setUuid(UUID.randomUUID().toString());
+        customerEntity.setFirstName(signupCustomerRequest.getFirstName());
+        customerEntity.setLastName(signupCustomerRequest.getLastName());
+        customerEntity.setEmail(signupCustomerRequest.getEmailAddress());
+        customerEntity.setContactNumber(signupCustomerRequest.getContactNumber());
+        customerEntity.setPassword(signupCustomerRequest.getPassword());
+        customerEntity.setSalt("abc1234");
+        final CustomerEntity createdCustomerEntity;
+        createdCustomerEntity=signupBusinessService.signup(customerEntity);
+        SignupCustomerResponse userResponse = new SignupCustomerResponse().id(createdCustomerEntity.getUuid()).status("CUSTOMER SUCCESSFULLY REGISTERED");
+        return new ResponseEntity<SignupCustomerResponse>(userResponse, HttpStatus.CREATED);
+    }
+
+
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
@@ -1,4 +1,55 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.regex.Pattern;
+
+@Service
 public class SignUpBusinessService {
+
+
+    @Autowired
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Autowired
+    private PasswordCryptographyProvider passwordCryptographyProvider = new PasswordCryptographyProvider();
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public CustomerEntity signup(CustomerEntity customerEntity) throws SignUpRestrictedException {
+
+        String[] encryptedText = passwordCryptographyProvider.encrypt(customerEntity.getPassword());
+        customerEntity.setSalt(encryptedText[0]);
+        customerEntity.setPassword(encryptedText[1]);
+        if (customerDao.getCustomerByContactNumber(customerEntity.getContactNumber()) != null) {
+            throw new SignUpRestrictedException("SGR-001", "This contact number is already registered! Try another contact number.");
+        } else if (!isValid(customerEntity.getEmail()))  {
+            throw new SignUpRestrictedException("SGR-002", "Invalid email-id format!");
+        } else if (customerEntity.getContactNumber() != "^$|[0-9]{10}") {
+            throw new SignUpRestrictedException("SGR-003", "Invalid contact number");
+        } else if (customerEntity.getPassword() != "(?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]).{8,16})" ){
+            throw new SignUpRestrictedException("SGR-004", "Weak password!");
+        } else if (customerEntity.getFirstName()== null || customerEntity.getEmail()== null || customerEntity.getContactNumber()== null || customerEntity.getPassword() == null) {
+            throw new SignUpRestrictedException("SGR-005", "Except last name all fields should be filled!");
+        } else {
+            return customerDao.createCustomer(customerEntity);
+        }
+    }
+
+
 }

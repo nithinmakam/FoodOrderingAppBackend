30fed02285b8ad0e58b03ebf8b1d3d42370ec065
emansi <mansielhance@gmail.com>
2019-09-12 01:45:16 +0530

    Sign up Endpoint - Completed Created, CustomerDao, CustomerController, SignUpBusinessService

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
@@ -1,4 +1,30 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAuthEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public class CustomerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public CustomerEntity createCustomer(CustomerEntity customerEntity){
+        entityManager.persist(customerEntity);
+        return customerEntity;
+    }
+
+
+    public CustomerEntity getCustomerByContactNumber(final String contactnumber){
+        try{
+            return entityManager.createNamedQuery("customerByContactNumber",CustomerEntity.class).setParameter("contactnumber",contactnumber).getSingleResult();
+        } catch (NoResultException nre){
+            return null;
+        }
+    }
 }

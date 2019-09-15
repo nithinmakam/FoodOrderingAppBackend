6b1c031ccbdd49e2fac864aa5594f661b62faa49
Vikas Garg <vikas04522@gmail.com>
2019-09-15 01:08:09 -0400

    added dao and business service file

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
@@ -0,0 +1,22 @@
+package com.upgrad.FoodOrderingApp.service.business;
+
+import com.upgrad.FoodOrderingApp.service.dao.PaymentDao;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    PaymentDao paymentDao;
+    @Transactional(propagation = Propagation.REQUIRED)
+    public List<PaymentEntity> getAllPaymentMethods(){
+        List<PaymentEntity> payment = paymentDao.getAllPaymentMethods();
+        return payment;
+    }
+}

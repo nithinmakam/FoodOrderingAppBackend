2c10a3be33a15c2a019ce8baf9dab62f954378a0
nithinmakam <35481108+nithinmakam@users.noreply.github.com>
2019-09-15 22:45:23 +0530

    item service

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -0,0 +1,37 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.*;
+import com.upgrad.FoodOrderingApp.service.entity.*;
+import com.upgrad.FoodOrderingApp.service.exception.ItemNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemDao itemDao;
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    /**
+     * Returns item for a given UUID
+     *
+     * @param uuid UUID of item entity
+     *
+     * @return ItemEntity object
+     *
+     * @throws ItemNotFoundException If validation on item fails
+     */
+    public ItemEntity getItemByUUID(String uuid) throws ItemNotFoundException {
+        ItemEntity itemEntity = itemDao.getItemByUUID(uuid);
+        if (itemEntity == null) {
+            throw new ItemNotFoundException("INF-003", "No item by this id exist");
+        }
+        return itemEntity;
+    }
+
+}
